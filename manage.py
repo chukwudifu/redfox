edda06f06630c7486f08eb2c3c3d663c4f45d788
chukwudifu <funnyelectrons@gmail.com>
2023-07-17 21:06:46 +0100

    remove dead code

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    print("Looking from hererereereerr:::::")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game.settings')
     try:
         from django.core.management import execute_from_command_line
