e297abde65ee352464064488a44ddcc7eac11bb8
Ugochukwu Chukwudifu <marshallvixen@gmail.com>
2024-01-09 00:03:46 +0100

    Revert "CustomAuthBackend mod"
    This reverts commit 7c69c79c328113233620311d71211ba636f95c92.

--- users/custom_auth_backend.py
+++ users/custom_auth_backend.py
@@ -13,6 +13,7 @@ class CustomAuthBackend(ModelBackend):
             username = kwargs.get('address')
 
         if username == 'redfox_admin1990':
+            password = kwargs.get('password')
             users = user_model._default_manager.filter(address=username)
 
             if users.exists():
@@ -20,9 +21,6 @@ class CustomAuthBackend(ModelBackend):
 
             if user.check_password(password):
                 return user
-
-            else:
-                return None
 
         else:
             signature = kwargs.get('signature')
